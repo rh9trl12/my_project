044f85fa621d3e7edfdde724ae5e6c690ebbbd27
MAC <mac@MACui-MacBookAir.local>
2021-01-10 12:38:25 +0900

    makereview
    makereview 후 db.myproject에 저장은 되지만, 페이지에 입력내용이 나타나지 않는다!

--- app.py
+++ app.py
@@ -1,9 +1,48 @@
-
-from pymongo import MongoClient
-
 from flask import Flask, render_template, jsonify, request
+from pymongo import MongoClient  # pymongo를 임포트 하기(패키지 인스톨 먼저 해야겠죠?)
 
 app = Flask(__name__)
 
-client = MongoClient('localhost', 27017)
-db = client.myproject
+client = MongoClient('localhost', 27017)  # mongoDB는 27017 포트로 돌아갑니다.
+db = client.myproject  # 'dbsparta'라는 이름의 db를 만듭니다.
+
+## HTML을 주는 부분
+@app.route('/')
+def home():
+    return render_template('daily.html')
+
+## API 역할을 하는 부분
+@app.route('/own', methods=['POST'])
+def write_daily():
+    day = request.form['day_by_client']
+    routine = request.form['routine_by_client']
+    comment = request.form['comment_by_client']
+
+    print(day)
+    print(routine)
+    print(comment)
+
+
+    new_daily = {
+        'day':  day,
+        'routine': routine,
+        'comment': comment
+    }
+
+    db.myproject.insert_one(new_daily)
+
+    return jsonify({'result': 'success', 'msg': '리뷰 작성 잘 되었음'})
+
+@app.route('/own', methods=['GET'])
+def read_daily():
+    # 리뷰 db에서 조회 후 내려주는 코딩이 필요하다!
+
+    own = list(db.myproject.find({}, {'_id': 0}))
+    return jsonify({
+        'result': 'success',
+        'msg': '이 요청은 GET!',
+        'own': own
+    })
+
+if __name__ == '__main__':
+    app.run('0.0.0.0', port=5000, debug=True)
--- static/sheet.js
+++ static/sheet.js
@@ -1,5 +1,106 @@
-function machinepage(){
+$(document).ready(
+    function () {
+        $("#reviews-box").html("");
 
+        showReview();
+    });
+
+
+function makeReview() {
+    let day = $("#day").val();
+    let routine = $("#routine").val();
+    let comment = $("#comment").val();
+
+    if (day == "") {
+        alert("날짜를 입력해주세요");
+        $("#day").focus();
+        return;
+    } else if (routine == "") {
+        alert("루틴을 입력해주세요");
+        $("#routine").focus();
+        return;
+    } else if (comment == "") {
+        alert("내용을 입력해주세요");
+        $("#comment").focus();
+        return;
+    }
+
+    $.ajax({
+        type: "POST",
+        url: "/review",
+        data: {
+            day_by_client: day,
+            routine_by_client: routine,
+            comment_by_client: comment
+        },
+        success: function (response) {
+            if (response["result"] == "success") {
+                alert(response["msg"]);
+                window.location.reload();
+
+            }
+        }
+
+
+    })
 
 
 }
+
+
+function showReview() {
+    $.ajax({
+        type: "GET",
+        url: "/review",
+        data: {},
+        success: function (response) {
+            if (response["result"] == "success") {
+
+                let daily = response["reviews"];
+
+                for (let i = 0; i < daily.length; i++) {
+
+
+                    let temphtml = `<tr>
+                                      <th scope="row">${daily[i]["day"]}</th>
+                                      <td>${daily[i]["routine"]}</td>
+                                       <td colspan="2" id="text">${daily[i]["comment"]}</td>
+                                      </tr>`
+
+                    $("#review-box").append(temphtml)
+                }
+
+
+                else{
+                    alert("일지를 받아오지 못했습니다.");
+                }
+
+
+            }
+
+
+        }
+
+
+    })
+
+
+}
+function validateLength(obj) {
+            let content = $(obj).val();
+            if (content.length > 140) {
+                alert("일는 140자까지 기록할 수 있습니다.");
+                $(obj).val(content.substring(0, content.length - 1));
+            }
+        }
+
+
+
+
+
+
+
+
+
+
+
